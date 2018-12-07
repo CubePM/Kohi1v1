4d19e55466f4be74631283e1413c1ebf64514799
Savion <saviondeavault@icloud.com>
2018-12-07 09:59:17 -0500

    Initial commit

---
+++ src/SavionLegends/Kohi1v1/Main.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SavionLegends\Kohi1v1;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+
+    public function onEnable(){
+
+    }
+
+    public function onDisable(){
+
+    }
+}
